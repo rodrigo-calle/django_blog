8f6bca30ced0e3bc559f1cea972a891504a14799
Rodrigo Calle <rodrigoc_0@hotmail.com>
2024-03-31 21:40:17 -0500

    chapter two: pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 # from django.http import Http404
+from django.core.paginator import Paginator
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Pagination with 3 posts per_page
+    paginator = Paginator(post_list, 2)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):

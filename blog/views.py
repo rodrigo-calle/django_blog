d7eaf8d4d4f1afade1c5b42a9f9c060104861b4a
Rodrigo Calle <rodrigoc_0@hotmail.com>
2024-03-31 21:53:28 -0500

    chapter two: adding view exceptions

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 # from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, \
+        PageNotAnInteger
 
 # Create your views here.
 def post_list(request):
@@ -9,7 +10,14 @@ def post_list(request):
     # Pagination with 3 posts per_page
     paginator = Paginator(post_list, 2)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
